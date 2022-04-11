daac535d1255724a3ebce16f0017a7c6295ca15b
jkenton21 <jkenton21@gmail.com>
2022-04-11 17:10:49 -0500

    tests file added, initial ship factory function added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 //Battleship JS Index File
 
 //Ship factory function
-const ship = () => {
-    
+function ship(length, startCoord, direction) {
+    return {
+        shipType: type,
+        shipCoords: shipArray,
+        shipHits: shipHitArray,
+        sunk: false,
+        hit(),
+        isSunk(),
+    };
 }
+
+//Hit function to mark ships position as hit
+function hit() {
+
+}
+
+//isSunk function to determine when a ship is sunk
+function isSunk() {
+
+}
+
---
+++ tests/battleship.tests.js
@@ -0,0 +1,1 @@
+// Unit tests for Battleship JS program
