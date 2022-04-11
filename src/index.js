5a078480ff59c64b0cb12549c988659d73b01aa4
jkenton21 <jkenton21@gmail.com>
2022-04-11 18:15:52 -0500

    ship functions for hitting and determining if sunk added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 //Battleship JS Index File
 
 //Ship factory function
-function ship(length, startCoord, direction) {
-    return {
-        shipType: type,
-        shipCoords: shipArray,
-        shipHits: shipHitArray,
-        sunk: false,
-        hit(),
-        isSunk(),
-    };
+function ship(type, length, coordinate) {
+        this.type = type;
+        this.length = length;
+        this.hitsArray = hit(this.length);
+        this.hit = function (number) {
+            this.hitsArray[number] = 'X';
+            this.isSunk = isSunk(this.hitsArray);
+        };
+        this.isSunk = false;
 }
 
-//Hit function to mark ships position as hit
-function hit() {
-
+//Hit function to create ship array
+function hit(length) {
+    const hitsArray = [];
+    for (let i =0; i < length; i++) {
+        hitsArray[i] = ``;
+    }
+    return hitsArray;
 }
 
 //isSunk function to determine when a ship is sunk
 function isSunk() {
-
+    if (hitsArray.every((element) => {
+        return element === 'X';
+    })
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+export default {isSunk, ship,}
 
